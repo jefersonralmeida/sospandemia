2cd347a9f2edc8eadb359556a7d57e332f4c1be2
João Augusto Grobe Castilho <joaocastilho@alunos.utfpr.edu.br>
2020-03-31 19:23:50 -0300

    Alterar demanda

---
+++ spa/src/components/views/Demand.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+  <modal :name="`hello-world-${demand.id}`">
+    <div class="container my-2">
+        <form class="ma-3">
+            <div class="form-group">
+                <label for="formEditTitle">Titulo</label>
+                <input v-model="tempDemand.title" id="formEditTitle" type="text" class="form-control" placeholder="Entre com o titulo">
+            </div>
+            <div class="form-group">
+                <label for="formEditDescription">Descrição</label>
+                <textarea v-model="tempDemand.text" type="text" class="form-control" id="formEditDescription" rows="4" style="resize: none"></textarea>
+            </div>
+            <button @click="handleUpdateDemand" class="btn btn-success float-right mx-2">Salvar</button>
+            <button @click="handleUpdateDemand" class="btn btn-danger float-right">Cancelar</button>
+        </form>
+    </div>
+  </modal>
+  <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ demand.title }}</h5>
+        <p class="card-text">{{ demand.text }}</p>
+      </div>
+        <div class="d-flex card-footer text-muted small" >
+            <div class="flex-grow-1 align-self-center">Quantidade: {{ demand.quantity }}</div>
+            <button class="btn btn-warning btn-sm ml-1" @click="showPopup">Alterar</button>
+            <button class="btn btn-danger btn-sm ml-1" @click="onDeleteDemandCB(demand.id)">Excluir</button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        demand: {
+            type: Object,
+            required: true
+        },
+        onUpdateDemandCB: {
+            type: Function,
+            required: true
+        },
+        onDeleteDemandCB: {
+            type: Function,
+            required: true
+        }
+    },
+    data(){
+        return{
+            tempDemand:{}
+        }
+    },
+    methods: {
+        showPopup() {
+            this.$modal.show(`hello-world-${this.demand.id}`);
+            this.tempDemand = JSON.parse(JSON.stringify(this.demand));
+        },
+        hidePopup() {
+            this.$modal.hide(`hello-world-${this.demand.id}`);
+        },
+        handleUpdateDemand(ev){
+            ev.preventDefault()
+
+            //Validar dados
+    
+            this.demand.title = this.tempDemand.title
+            this.demand.text = this.tempDemand.text
+
+            this.onUpdateDemandCB(this.demand.id, this.demand)
+            this.hidePopup();
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- spa/src/components/views/ManageDemandsLocal.vue
+++ spa/src/components/views/ManageDemandsLocal.vue
@@ -1,11 +1,54 @@
 <template>
   <div class="hello">
     <h1>Gerenciar Demandas</h1>
-    <p>Listar aqui as demandas da entidade <strong>---</strong></p>
+    <p>
+      Listar aqui as demandas da entidade
+      <strong>---</strong>
+    </p>
     <p>Lista paginada... Incluir opções de incluir, editar, excluir, resolver..., CRUD completo. Esqueleto abaixo, respostas no console.</p>
-    <button class="btn btn-success" @click="createDemand()">Nova demanda</button>
-    <hr/>
-    <div class="card" v-for="demand in demands">
+    <button class="btn btn-success" @click="creatingDemand = true">Nova demanda</button>
+    <div v-if="creatingDemand">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Titulo da demanda</label>
+          <input
+            v-model="demandTemp.title"
+            type="text"
+            class="form-control"
+            placeholder="Titulo da demanda"
+          />
+          <small id="emailHelp" class="form-text text-muted">{{demandTemp.title}}</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+
+    <hr />
+    <demand-card
+      v-for="(demand, i) in demands"
+      :key="i"
+      :demand="demand"
+      :onViewDemandCB="viewDemand"
+      :onUpdateDemandCB="updateDemand"
+      :onDeleteDemandCB="deleteDemand"
+      class="mt-2"
+    ></demand-card>
+
+    <!--
+    <div class="card" v-for="(demand, i) in demands" v-bind:key="i">
       <div class="card-body">
         <h5 class="card-title">{{ demand.title }}</h5>
         <p class="card-text">{{ demand.text }}</p>
@@ -15,17 +58,27 @@
         <button class="btn btn-danger" @click="deleteDemand(demand.id)">Excluir</button>
       </div>
     </div>
+    -->
   </div>
 </template>
 
 <script>
 import api from "../../api_local";
 import randomstring from "randomstring";
+import Demand from "./Demand";
 
 export default {
-  name: 'ManageDemandsLocal',
+  name: "ManageDemandsLocal",
+  components: {
+    "demand-card": Demand
+  },
   data: () => ({
-    demands: []
+    demands: [],
+    creatingDemand: false,
+    demandTemp: {
+      title: "",
+      text: ""
+    }
   }),
   methods: {
     loadDemands: function() {
@@ -36,33 +89,26 @@ export default {
     },
     createDemand: function() {
       const data = {
-        title: 'New demand ' + randomstring.generate(5),
-        text: 'Test description ' + randomstring.generate(5),
+        title: "New demand " + randomstring.generate(5),
+        text: "Test description " + randomstring.generate(5)
       };
       api.createDemand(data).then(() => {
         this.loadDemands();
       });
     },
     viewDemand: function(demandId) {
-      api.getDemand(demandId).then(({data}) => {
+      api.getDemand(demandId).then(({ data }) => {
         console.log(data);
       });
     },
-    updateDemand: function(demandId) {
-
+    updateDemand: function(demandId, data) {
       // estou apenas pegando o valor atual...
-      const current = this.demands.find((demand) => demand.id === demandId);
+      const current = this.demands.find(demand => demand.id === demandId);
 
-      // e adicionando um - ao final do título a cada update, e incrementando a quantidade
-      const data = {
-        title: current.title + '-',
-        quantity: current.quantity === null ? 1 : ++current.quantity,
-      };
       api.updateDemand(demandId, data).then(response => {
         console.log(response);
         this.loadDemands();
       });
-
     },
     deleteDemand: function(demandId) {
       api.deleteDemand(demandId).then(response => {
@@ -83,11 +129,10 @@ export default {
   },
   mounted() {
     this.loadDemands();
-  },
-}
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
