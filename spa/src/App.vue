458313a60fea37032e1107d9bd27b0725f7641b9
João Augusto Grobe Castilho <joaocastilho@alunos.utfpr.edu.br>
2020-04-06 18:23:08 -0300

    Corrigido problema de cores Vuetify

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -1,47 +1,50 @@
 <template>
+  <v-app>
     <div id="app" class="container-fluid h-100 w-100 p-0">
-        <component :is="layout"></component>
+      <component :is="layout"></component>
     </div>
+  </v-app>
 </template>
 
 <script>
-    import MainLayout from "./components/layouts/MainLayout";
-    import CompactLayout from "./components/layouts/CompactLayout";
-    import NoLayout from "./components/layouts/NoLayout";
+import MainLayout from "./components/layouts/MainLayout";
+import CompactLayout from "./components/layouts/CompactLayout";
+import NoLayout from "./components/layouts/NoLayout";
 
-    const default_layout = 'main-layout';
-    export default {
-        name: 'App',
-        components: {
-            MainLayout,
-            CompactLayout,
-            NoLayout,
-        },
-        data() {
-            return {}
-        },
-        computed: {
-            layout() {
-                return (this.$route.meta.layout || default_layout);
-            },
-        },
-        mounted() {
-            if (!this.$store.profile && this.$store.getters.isLogged) {
-                this.$store.dispatch('loadProfile');
-            }
-        },
+const default_layout = "main-layout";
+export default {
+  name: "App",
+  components: {
+    MainLayout,
+    CompactLayout,
+    NoLayout
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    layout() {
+      return this.$route.meta.layout || default_layout;
     }
+  },
+  mounted() {
+    if (!this.$store.profile && this.$store.getters.isLogged) {
+      this.$store.dispatch("loadProfile");
+    }
+  }
+};
 </script>
 
 <style>
-    html, body {
-        height: 100%;
-    }
+html,
+body {
+  height: 100%;
+}
 
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        color: #2c3e50;
-    }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
 </style>
--- spa/src/main.js
+++ spa/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import VModal from 'vue-js-modal'
 import router from './router.js'
 import store from './store.js'
-import Vuetify from "vuetify";
+import vuetify from './plugins/vuetify';
+
 
 
 import 'bootstrap'
@@ -13,7 +14,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import "vuetify/dist/vuetify.min.css";
 
-Vue.use(Vuetify);
 
 Vue.use(VModal)
 
@@ -22,5 +22,6 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
   store,
-  router
+  router,
+  vuetify
 }).$mount('#app');
