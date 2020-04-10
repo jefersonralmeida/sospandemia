e37904ccd86bfe96382e07ffce2c1dd76086966c
Jeferson Almeida <jeferson.almeida@codelathe.com>
2020-04-10 10:36:12 -0300

    - Correcoes na build - Incluido package AWS\SES

--- build.sh
+++ build.sh
@@ -6,20 +6,17 @@ dcp="docker-compose -f docker-compose-prod.yml";
 echo '### ENSURE THE DEV CONTAINERS ARE DOWN ###';
 $dc down
 
-echo '### BRING THE DEV ENV UP ###'
-$dc up -d
-
 echo '### INSTALL SPA DEPENDENCIES ###'
 $dc exec spa npm install
 
 echo '### BUILD THE SPA DIST VERSION ###'
-$dc exec spa npm run build --mode=production
+$dc run spa npm run build --mode=production
 
 echo '### ENSURE THAT THE API BUILD FOLDER EXISTS ###'
-$dc exec api mkdir -p /var/dist/build
+$dc run api mkdir -p /var/dist/build
 
 echo '### COPY FILES TO THE API BUILD FOLDER ###'
-$dc exec api rsync -av --delete . /var/dist/build \
+$dc run api rsync -av --delete . /var/dist/build \
 --exclude vendor \
 --exclude node_modules \
 --exclude storage/* \
@@ -30,23 +27,20 @@ $dc exec api rsync -av --delete . /var/dist/build \
 --exclude webpack.mix.js;
 
 echo '### MANAGE .env FILES ###'
-$dc exec api rm -f /var/dist/build/.env*;
-$dc exec api cp ./.env.prod /var/dist/build/.env;
+$dc run api rm -f /var/dist/build/.env*;
+$dc run api cp ./.env.prod /var/dist/build/.env;
 
 
 echo '### INSTALL API NPM DEPENDENCIES ###'
-$dc exec api npm install --cwd=/var/dist/build --prefix=/var/dist/build
+$dc run api npm install --cwd=/var/dist/build --prefix=/var/dist/build
 
 echo '### BUILD THE API ASSETS ###'
-$dc exec api npm run prod --cwd=/var/dist/build --prefix=/var/dist/build
+$dc run api npm run prod --cwd=/var/dist/build --prefix=/var/dist/build
 
 echo '### EXECUTE COMPOSER ON API ###'
-$dc exec api composer install --working-dir=/var/dist/build --no-ansi --no-dev --no-interaction --no-progress --no-suggest --optimize-autoloader
+$dc run api composer install --working-dir=/var/dist/build --no-ansi --no-dev --no-interaction --no-progress --no-suggest --optimize-autoloader
 
-rem TODO - generate .env, run migrations, compile routes and view, generate key pair, run indexing remove artisan, remove migrations, remove routes, remove views
-
-echo '### BRING THE DEV ENV DOWN ###'
-rem bring the dev env down
+echo '### REMOVE TEMPORARY CONTAINERS ###'
 $dc down
 
 echo '### BUILD THE PROD ENV ###'
@@ -61,4 +55,4 @@ $dcp run p_api php artisan view:cache
 echo '### CACHING THE ROUTES ###'
 $dcp run p_api php artisan route:cache
 
-
+# TODO - bring the env up, run migrations, compile routes and view, generate key pair, run indexing remove artisan, remove migrations, remove routes, remove views
