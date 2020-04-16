116a51ded96e3fbfdc7802a1ff3163a14c21c487
Edson Silva <edsonsilvajr21@gmail.com>
2020-04-16 17:16:33 -0300

    layout da rota Equipe

---
+++ spa/src/components/views/Equipe.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="equipe">
+    <section class="text-center">
+      <div class="container">
+        <h1>SOS Pandemia</h1>
+        <p class="lead text-muted">Adicionar texto</p>
+      </div>
+    </section>
+    <div class="album py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1>Devs</h1>
+          </div>
+          <div class="col-4">
+            <div class="card mb-4 shadow-sm">
+              <img class="card-img-top" src="../../assets/logo_app.png" alt="Card image cap" />
+              <div class="card-body">
+                <h2>nome aqui</h2>
+                <p class="card-text">Informações Aqui</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <h1>Devs</h1>
+          </div>
+          <div class="col-4">
+            <div class="card mb-4 shadow-sm">
+              <img class="card-img-top" src="../../assets/logo_app.png" alt="Card image cap" />
+              <div class="card-body">
+                <h2>nome aqui</h2>
+                <p class="card-text">Informações Aqui</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
