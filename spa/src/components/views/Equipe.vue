6fee024c5ac8078072a0b0ebb5fd9d3020cf063f
Hernani Cruz <hernanicruz@hotmail.com>
2020-04-16 20:21:09 -0300

    Novas correções equipe

--- spa/src/components/views/Equipe.vue
+++ spa/src/components/views/Equipe.vue
@@ -11,7 +11,7 @@
           
       <p class="lead text-muted">Estamos passando por uma crise pandêmica com forte impacto social e econômico, de forma que é considerado um momento único e desafiador em que devemos praticar o estar seguro, estar saudável e estar positivo. Toda ideia para conter ou mitigar esta crise é muito bem-vinda e tem como o objetivo de garantir o bem-estar social da humanidade. A pandemia em que nos referimos é o COVID-19 que significa Coronavirus Disease 2019.</p>
       <p class="lead text-muted">O SOS Pandemia é uma iniciativa solidária que visa mitigar a expansão desta pandemia através de uma infraestrutura de comunicação que visa integrar as unidades dos sistemas de saúde a fim de que os recursos disponíveis possam atender as demandas de forma eficiente para que possamos ter o máximo de vidas salvas.</p>
-      <p class="lead text-muted">O sistema desenvolvido e denotado por SOS Pandemia é caracterizado por uma plataforma de infraestrutura web responsiva para dispositivos móveis, possibilita aos usuários uma interface simples e fácil de operação com garantia de ações rápidas.  Os recursos e demandas são possíveis de serem rastreados, controlador e gerenciados em tempo real com informações de status e uma integração sistemas de almoxarifado, manutenção e compras. Consequentemente, o controle de fluxo de equipamentos e componentes pode ser melhor utilizado.</p>
+      <p class="lead text-muted">O sistema desenvolvido e denotado por SOS Pandemia é caracterizado por uma plataforma de infraestrutura web responsiva para dispositivos móveis, possibilita aos usuários uma interface simples e fácil de operação com garantia de ações rápidas.  Os recursos e demandas são possíveis de serem rastreados, controlados e gerenciados em tempo real com informações de status e uma integração sistemas de almoxarifado, manutenção e compras. Consequentemente, o controle de fluxo de equipamentos e componentes pode ser melhor utilizado.</p>
       <p class="lead text-muted">Um exemplo de aplicação em que o SOS Pandemia surge como uma iniciativa de boa utilização, é o caso dos respiradores artificiais. Estima-se que existam cerca de 3,6 mil respiradores pulmonares no Brasil que demandam por algum tipo de manutenção. Estes equipamentos são essenciais no tratamento de doentes em estado grave do COVID-19, pois provocam insuficiência respiratória. Profissionais voluntários e capacitados das montadoras e Senai são capazes de realizar a manutenção destes equipamentos. Sendo assim, o SOS Pandemia pode auxiliar na melhor logística entre local que contém equipamento que precise de reparos e pontos de manutenção habilitados em diversas regiões do Brasil. Desta forma, o SOS Pandemia possibilita reduzir o tempo de manutenção e disponibilidade operacional do respirador a pacientes que demandam deste recurso de imediato.  </p>
       <p class="lead text-muted">Somos uma parceria entre a UTFPR Câmpus de Ponta Grossa e o Programa de Ciências da Saúde e Hospital Universitário da UEPG. Nossa equipe é composta pela: coordenadora Sani Rutz, professor Max Mauro Dias Santos, os alunos Diego F. Schlosser, Hernani Cruz, Edson da Silva Júnior, João Castilho; além do proprietário da Web Age Informática, Jeferson Almeida; e do professor Ricardo Zanetti, coordenador do curso de Medicina da UEPG.</p>
       <p class="lead text-muted">Convidamos todas as instâncias a fazer parte dessa iniciativa!</p>
@@ -27,10 +27,8 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" src="../../assets/joao.jpeg" alt="Card image cap">
                 <div class="card-body">
-                  <small class="text-muted">João Algusto Castilho</small>
-                  <br>
+                  <a href="#" class="card-link"><h5 class="card-title">João Algusto Castilho</h5></a>
                   <small class="text-muted">Cursando Ciência da Computação na UTFPR.</small>
-                  <!--<p class="card-text">Cursando Ciência da Computação na UTFPR.</p>-->
                   
                 </div>
               </div>
@@ -39,8 +37,7 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" src="../../assets/edson.jpg" alt="Card image cap">
                 <div class="card-body">
-                  <small class="text-muted">Edson Luiz</small>
-                  <br>
+                  <a href="#" class="card-link"><h5 class="card-title">Edson Luiz</h5></a>
                   <small class="text-muted">Cursando Ciência da Computação na UTFPR</small>
                 </div>
               </div>
@@ -49,8 +46,7 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" src="../../assets/jeferson.jpeg" alt="Card image cap">
                 <div class="card-body">
-                  <small class="text-muted">Jeferson Rodrigo Almeida</small>
-                  <br>
+                  <a href="#" class="card-link"><h5 class="card-title">Jeferson Rodrigo Almeida</h5></a>
                   <small class="text-muted">Graduação em Engenharia da Computação - UEPG,  possui MBA em Gestão de Projetos e proprietário da empresa WebAge Solutions.</small>
                 </div>
               </div>
@@ -60,8 +56,7 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" src="../../assets/hernani.jpg" alt="Card image cap">
                 <div class="card-body">
-                  <small class="text-muted">Hernani Batista da Cruz</small>
-                  <br>
+                  <a href="http://lattes.cnpq.br/6098604503176841" target="_blank" class="card-link"><h5 class="card-title">Hernani Batista da Cruz</h5></a>
                   <small class="text-muted">Bacharel em Física e Tecnólogo em Sistemas para Internet. Mestre em Ensino de Física e Doutorando em Ensino de Ciência e Tecnologia - UTFPR.</small>
                  
                 </div>
@@ -71,8 +66,7 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" src="../../assets/diego.jpeg" alt="Card image cap">
                 <div class="card-body">
-                  <small class="text-muted">Diego Schlosser </small>
-                  <br>
+                  <a href="#" target="_blank" class="card-link"><h5 class="card-title">Diego Schlosser</h5></a>
                   <small class="text-muted">Graduado em Análise e Desenvolvimento de Sistemas pela Universidade Tecnológica Federal do Paraná - Câmpus Medianeira. Especialista em Tecnologias da Informação e Comunicação na Educação. Mestre em Ensino de Ciência e Tecnologia pela Universidade Tecnológica Federal do Paraná, Câmpus Ponta Grossa.</small>
                  
                 </div>
@@ -82,9 +76,10 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" src="../../assets/sani.jpeg" alt="Card image cap">
                 <div class="card-body">
-                  <small class="text-muted">Sani de Carvalho Rutz da Silva </small>
+
+                  <a href="http://lattes.cnpq.br/5694972079639390" target="_blank" class="card-link"><h5 class="card-title">Sani de Carvalho Rutz da Silva</h5></a>
                   <br>
-                  <small class="text-muted">.</small>
+                  <small class="text-muted">Licenciada em Matemática (UEPG), Mestre em Matemática Aplicada e Doutora em Ciência dos Materiais pela Universidade Federal do Rio Grande do Sul (UFRGS). Em pesquisas, tem como ênfase as temáticas: Ensino de Matemática, Educação Matemática Inclusiva, Uso de Tecnologias no ensino de Matemática. Recebeu o Prêmio Tese de CAPES (orientador) no ano de 2018, melhor tese na Área de Ensino, intitulada O Soroban na formação de conceitos matemáticos por pessoas com deficiência intelectual: implicações na aprendizagem e no desenvolvimento. Editor Chefe da Revista Brasileira de Ensino de Ciência e Tecnologia (RBECT).</small>
                  
                 </div>
               </div>
@@ -96,8 +91,7 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" src="../../assets/ricardo.jpg" alt="Card image cap">
                 <div class="card-body">
-                  <small class="text-muted">Ricardo Zanetti Gomes</small>
-                  <br>
+                  <a href="#" target="_blank" class="card-link"><h5 class="card-title">Ricardo Zanetti Gomes</h5></a>
                   <small class="text-muted">.</small>
                  
                 </div>
@@ -107,8 +101,7 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" src="../../assets/max.jpg" alt="Card image cap">
                 <div class="card-body">
-                  <small class="text-muted">Max Mauro</small>
-                  <br>
+                  <a href="#" target="_blank" class="card-link"><h5 class="card-title">Max Mauro</h5></a>
                   <small class="text-muted">.</small>
                  
                 </div>
