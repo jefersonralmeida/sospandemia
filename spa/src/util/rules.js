0a7479928c29badcc24c1b9b36f485597c57ca03
Roppwer <joaocastilho@alunos.utfpr.edu.br>
2020-04-20 15:40:20 -0300

    Alteração da regra de números
    Utilização de expressões regulares para detectar números.
    Diferentes mensagens de erros, diferenciando apenas numeros; e numero menor ou igual a zero.

--- spa/src/util/rules.js
+++ spa/src/util/rules.js
@@ -5,7 +5,12 @@ const rules = {
                 required: value => !!value || "Obrigatório.",
                 min: (v, num = 8) => v != null && v.length >= num || `Minimo ${num} caracteres`,
                 max: (v, num = 100) => v != null && v.length <= num || `Máximo ${num} caracteres`,
-                numberRule: v => (parseInt(v) && v >= 1) || "O campo deve conter apenas números."
+                numberRule: v => {
+                    const regexNumbers = new RegExp(/^-?[0-9]*$/)
+                    if(!regexNumbers.test(parseFloat(v))) return "O campo deve conter apenas números."
+                    if(v <= 0) return "O campo deve conter valores maiores que zero."
+                    return true
+                }
             }
         }
     }
