ee93323802d1401aa578a0bad863e0c0c702687f
Edson Silva <edsonsilvajr21@gmail.com>
2020-04-20 18:29:49 -0300

    alterado popup da demanda

--- spa/src/components/views/Demand.vue
+++ spa/src/components/views/Demand.vue
@@ -1,45 +1,54 @@
 <template>
   <div>
-    <modal :name="`updateModal-${demand.id}`" :adaptive="false" height="450px">
-      <div class="container mt-2">
-        <v-form class="ma-3" ref="form">
-          <div class="form-group">
-            <v-text-field
-              v-model="tempDemand.title"
-              :rules="[rules.required]"
-              label="Demanda"
-              outlined
-            ></v-text-field>
-          </div>
-          <div class="form-group">
-            <v-text-field
-              type="number"
-              v-model="tempDemand.quantity"
-              :rules="[rules.numberRule,rules.required]"
-              label="Quantidade"
-              outlined
-            ></v-text-field>
-          </div>
-          <div class="form-group">
-            <v-textarea
-              rows="3"
-              auto-grow
-              label="Adicione um descrição"
-              outlined
-              v-model="tempDemand.text"
-              :rules="[rules.required]"
-            ></v-textarea>
-          </div>
+    <v-dialog v-model="update" max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Alterar Demanda</span>
+        </v-card-title>
+        <v-card-text class="py-0">
+          <v-container>
+            <v-form ref="form">
+              <div class="form-group">
+                <v-text-field
+                  v-model="tempDemand.title"
+                  :rules="[rules.required]"
+                  label="Demanda"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="form-group">
+                <v-text-field
+                  type="number"
+                  v-model="tempDemand.quantity"
+                  :rules="[rules.numberRule,rules.required]"
+                  label="Quantidade"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="form-group">
+                <v-textarea
+                  rows="4"
+                  label="Adicione um descrição"
+                  outlined
+                  v-model="tempDemand.text"
+                  :rules="[rules.required]"
+                ></v-textarea>
+              </div>
+            </v-form>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="update=false" class="float-right" color="red" dark>Cancelar</v-btn>
           <v-btn
             @click="handleUpdateDemand"
             color="success"
             class="float-right mx-2"
             :loading="loading"
           >Salvar</v-btn>
-          <button @click="handleUpdateCancellDemand" class="btn btn-danger float-right">Cancelar</button>
-        </v-form>
-      </div>
-    </modal>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
     <modal :name="`deleteModal-${demand.id}`" :width="300" :height="150">
       <div class="container my-2 text-center">
@@ -52,7 +61,7 @@
           <v-btn
             @click="handleRemoveDemandConfirm"
             color="red darken-3"
-            class="float-right mx-2  white--text"
+            class="float-right mx-2 white--text"
             :loading="loading"
           >Remover</v-btn>
         </div>
@@ -62,13 +71,13 @@
     <div class="card">
       <div class="card-body">
         <div class="card-title">
-        <h5>{{ demand.title }}</h5>
+          <h5>{{ demand.title }}</h5>
         </div>
         <p class="card-text">{{ demand.text }}</p>
       </div>
       <div class="d-flex card-footer text-muted small">
         <div class="flex-grow-1 align-self-center">Quantidade: {{ demand.quantity }}</div>
-        <button class="btn btn-warning btn-sm ml-1" @click="showPopup('updateModal')">Alterar</button>
+        <button class="btn btn-warning btn-sm ml-1" @click="openUpdatePopup">Alterar</button>
         <button class="btn btn-danger btn-sm ml-1" @click="handleRemoveDemand">Excluir</button>
       </div>
     </div>
@@ -96,7 +105,8 @@ export default {
   data() {
     return {
       tempDemand: {},
-      loading: false,
+      update: false,
+      loading: false
     };
   },
   methods: {
@@ -104,6 +114,10 @@ export default {
       this.$modal.show(`${ModelName}-${this.demand.id}`);
       if (ModelName === "updateModal")
         this.tempDemand = JSON.parse(JSON.stringify(this.demand));
+    },
+    openUpdatePopup() {
+      this.tempDemand = JSON.parse(JSON.stringify(this.demand));
+      this.update = true;
     },
     validate() {
       console.log(this.$refs);
@@ -123,13 +137,22 @@ export default {
     },
     handleRemoveDemandConfirm(ev) {
       this.loading = true;
-      this.onDeleteDemandCB(this.demand.id).then(() => {
-        this.$store.commit('showMessage', { content:"Demanda removida com sucesso!", error:false })
-      }).catch(e => {
-        this.$store.commit('showMessage', { content:"Erro ao excluir demanda.", error:true })
-      }).finally(()=> {
-        this.loading = false;
-      })
+      this.onDeleteDemandCB(this.demand.id)
+        .then(() => {
+          this.$store.commit("showMessage", {
+            content: "Demanda removida com sucesso!",
+            error: false
+          });
+        })
+        .catch(e => {
+          this.$store.commit("showMessage", {
+            content: "Erro ao excluir demanda.",
+            error: true
+          });
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     },
     handleUpdateDemand(ev) {
       ev.preventDefault();
@@ -143,13 +166,20 @@ export default {
         this.onUpdateDemandCB(this.demand.id, this.demand)
           .then(ev => {
             this.hidePopup("updateModal");
-            this.$store.commit('showMessage', { content:"Demanda salva!", error:false })
+            this.$store.commit("showMessage", {
+              content: "Demanda salva!",
+              error: false
+            });
           })
           .catch(err => {
-            this.$store.commit('showMessage', { content:"Erro ao alterar demanda.", error:true })
+            this.$store.commit("showMessage", {
+              content: "Erro ao alterar demanda.",
+              error: true
+            });
           })
           .finally(() => {
             this.loading = false;
+            this.update = false;
           });
       }
     }
